66d3549f37af1d0f4037962c9aa8f7b67e5c8d5e
Zeeshan Desai <desaizeeshan22@gmail.com>
2020-11-26 23:40:12 -0600

    Add files via upload

---
+++ photos.sql
@@ -0,0 +1,7 @@
+CREATE TABLE photos(
+id INT AUTO_INCREMENT PRIMARY KEY,
+image_url VARCHAR(255),
+user_id INT NOT NULL,
+created_at TIMESTAMP DEFAULT NOW(),
+FOREIGN KEY(user_id) REFERENCES users(id) 
+);
